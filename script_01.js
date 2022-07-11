7301d4551f01f0529b31bd5a7c1be16bb3a5ebb1
Cing73 <craig@vancleaf>
2022-07-11 10:23:07 +0200

    first

--- script_01.js
+++ script_01.js
@@ -2,3 +2,96 @@
 
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assignment
+const ageJohn = 29;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark); 
+isJohnEqual = (ageJohn == ageMark); 
+
+// Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
+/************ IF ************/
+// TINA  --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+// if (true)
+// if (false)
+// if (isJohnOlder)
+// {
+//     console.log("John ist älter!");
+// }
+
+/************ IF - Else  ************/
+// mit alternative
+// entweder ja oder nein
+// if (isJohnOlder)
+// {
+//     console.log("John ist älter!");
+// }
+// else 
+// {
+//     console.log("John ist jünger");
+// }
+
+/************ Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older")
+
+/************ IF  -  ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+// if (isJohnOlder) // 1. Variante
+// {
+//     console.log("John ist älter!");
+// }
+// else if (isJohnEqual) // 2. Fall
+// {
+//     console.log("John ist gleich alt!")
+// }
+// // evtl. weitere Alternative(n) else if() {}
+// else // gemeinsamer else-zweig
+// {
+//     console.log("John ist jünger");
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+const firstName = "Jane";
+let job;
+
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein! 
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+job = "instructor";  // .. unterrichtet!
+switch (job) 
+{
+    case "driver": // Test auf ==
+        console.log(firstName + " fährt TAXI! / UBER")
+        break;
+        case "diver": // Test auf ==
+        console.log(firstName + " taucht im Rhein!")
+        break;
+        case "artist": // Test auf ==
+        console.log(firstName + " malt ein Bild!")
+        break;
+        case "teacher": // Test auf ==
+        case "instructor": // Test auf ==
+        console.log(firstName + " unterrichtet!")
+        break;
+
+
+
+        default:
+        console.log(firstName + " macht etwas anderes!")
+        break;
+}
